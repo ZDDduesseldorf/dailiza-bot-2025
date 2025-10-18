c77625868d61281855b166489e842545bfe9b37a
yassin <yassin.lebane@study.hs-duesseldorf.de>
2025-10-18 09:54:25 +0200

    Ich bin... pattern hinzugefügt

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -14,7 +14,9 @@ psychobabble = [
     "Bist du sicher, dass du {0} brauchst?"]],
 
     # Eigene
-    [r"Ich habe (.*)" , ["Warum hast du {0}?", "Wie hast du {0}?", "Wofür hast du {0}?"]]
+    [r"Ich habe (.*)" , ["Warum hast du {0}?", "Wie hast du {0}?", "Wofür hast du {0}?"]],
+
+    [r"Ich bin (.*)" , ["Bist du {0}, oder ist {0} du?", "Bist du {0}, oder fühlst du dich {0}?", "Denkst du ich bin auch {0}?"]]
 
 ]
 #python -m dailizabot.main
