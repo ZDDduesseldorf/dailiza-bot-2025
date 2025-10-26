3281b620ac0554b71759370c8cbda4092bb6115e
unknown <thorben.linzmaier@web.de>
2025-10-26 14:51:17 +0100

    Alle 3 Regex Muster aufeinmal hinzugefuegt

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -13,4 +13,18 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r".*Wetter.*ist(.{0,25})",
+     ["Ich finde auch das Wetter ist heute {0}",
+      "Beeinflusst dich das Wetter wenn es {0} ist?",
+      "Ich bin ein Chatbot für mich ist das Wetter immer {0}"]],
+    
+    [r".*machst.*heute(.*)",
+    ["Dasselbe wie jeden Tag ich bin immernoch ein Chatbot",
+     "Heute dasselbe wie gestern und morgen...",
+     "AWS server hacken wie letzte Woche"]],
+
+    [r".*(Gefallen|helfen)(.*)",
+    ["Ja klar wie kann ich dir weiterhelfen?",
+    "Ich bin gerade leider etwas beschäftigt",
+    "{1} ist etwas kniffliges aber das kriegen wir schon hin"]]
 ]
