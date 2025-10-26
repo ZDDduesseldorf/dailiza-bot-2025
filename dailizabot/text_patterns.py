f9c99e8c12b46fcbdd68696a061178a80fff41af
Oleksii Zvirkovskyi <oleksii.zvirkovskyi@study.hs-duesseldorf.de>
2025-10-26 17:16:34 +0100

    Regex "was kannst du" hinzugefügt

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -13,4 +13,10 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r"Was kannst du (.*)",
+    ["Ich kann mit dir über verschiedene Themen sprechen und dir zuhören.",
+     "Ich kann mit dir über vieles sprechen.",
+     "Da ich ein Chatbot bin, kann ich nur mit dir sprechen.",
+     "Ich kann fast alles - was willst du eigentlich??"]]
+
 ]
