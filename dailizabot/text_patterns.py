82af347fbe3b4c28b88f18d42505445ebc969316
Meriem Bouazza <meriem.bouazzastudy.hs-duesseldorf.de>
2025-10-28 21:27:26 +0100

    Thema Kompliment hinzugefügt

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -13,8 +13,14 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
-    [r"(?i)\b(du\s+bist\s+(doof|idiot|langsam|dumm|bescheuert|Opfer))\b",
+    [r"(?i)\bdu\s+bist\s+(doof|idiot|langsam|dumm|bescheuert|Opfer)\b",
      ["Aua, das tut mir Leid. Kannst du mir das genauer erklären?",
-      "Ich gebe mein Bestes :()",
+      "Ich gebe mein Bestes :(",
       "Oh, das ist aber gemein! Vielleicht hab ich's falsch verstanden."   ]],
+
+    [r"(?i)\bdu\s+bist\s+(schön|wundervoll|schlau|hübsch|lieb|nett|intelligent)\b",
+     ["Danke! Du bist auch {0}!",
+      "Das ist lieb und das kann ich nur zurück geben.",
+      "Du schmeichelst mir...",
+      ]],  
 ]
