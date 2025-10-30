e9ecb4eb174759fcfcf9c8407d1733915e9ecd27
Theo Zech <theo.ze@web.de>
2025-10-30 11:13:34 +0100

    Hilfestellungs Regex hinzugefügt

--- dailizabot/text_patterns.py
+++ dailizabot/text_patterns.py
@@ -13,4 +13,9 @@ psychobabble = [
     "Würde {0} dir denn wirklich helfen?",
     "Bist du sicher, dass du {0} brauchst?"]],
 
+    [r"kannst du mir bei (.*) helfen",
+    ["Sehr gerne helfe ich dir bei {0} !",
+     "Wo genau benötigst du bei {0} hilfe ?",
+     "Chatgbt kann dir im Notfall auch bei {0} helfen !"]]
+
 ]
